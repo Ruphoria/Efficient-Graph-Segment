90855cf7c47e20ab63e1333cc8aed9e7733df83d
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2020-11-11 20:20:51 +0000

    testing code

--- include/graph-segment/filter.h
+++ include/graph-segment/filter.h
@@ -67,3 +67,27 @@ static image<float> *smooth(image<float> *src, float sigma) {
   image<float> *dst = new image<float>(src->width(), src->height(), false);
   convolve_even(src, tmp, mask);
   convolve_even(tmp, dst, mask);
+
+  delete tmp;
+  return dst;
+}
+
+/* convolve image with gaussian filter */
+image<float> *smooth(image<uchar> *src, float sigma) {
+  image<float> *tmp = imageUCHARtoFLOAT(src);
+  image<float> *dst = smooth(tmp, sigma);
+  delete tmp;
+  return dst;
+}
+
+/* compute laplacian */
+static image<float> *laplacian(image<float> *src) {
+  int width = src->width();
+  int height = src->height();
+  image<float> *dst = new image<float>(width, height);  
+
+  for (int y = 1; y < height-1; y++) {
+    for (int x = 1; x < width-1; x++) {
+      float d2x = imRef(src, x-1, y) + imRef(src, x+1, y) -
+	2*imRef(src, x, y);
+      float d2y = imRef(src, x, y-1) + imRef(src, x, y+1) -
