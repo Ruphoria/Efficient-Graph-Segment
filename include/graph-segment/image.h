c2814435cd9df95d0af61f8654038324a28d0ef8
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2020-11-19 08:39:35 +0000

    Cleanup old files

--- include/graph-segment/image.h
+++ include/graph-segment/image.h
@@ -64,3 +64,28 @@ class image {
 template <class T>
 image<T>::image(const int width, const int height, const bool init) {
   w = width;
+  h = height;
+  data = new T[w * h];  // allocate space for image data
+  access = new T*[h];   // allocate space for row pointers
+  
+  // initialize row pointers
+  for (int i = 0; i < h; i++)
+    access[i] = data + (i * w);  
+  
+  if (init)
+    memset(data, 0, w * h * sizeof(T));
+}
+
+template <class T>
+image<T>::~image() {
+  delete [] data; 
+  delete [] access;
+}
+
+template <class T>
+void image<T>::init(const T &val) {
+  T *ptr = imPtr(this, 0, 0);
+  T *end = imPtr(this, w-1, h-1);
+  while (ptr <= end)
+    *ptr++ = val;
+}
