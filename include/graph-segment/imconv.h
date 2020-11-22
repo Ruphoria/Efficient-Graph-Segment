83928bb8d12f6aaca86a2b8141342625531884f9
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2020-11-22 15:33:32 +0000

    Fixed space

--- include/graph-segment/imconv.h
+++ include/graph-segment/imconv.h
@@ -43,3 +43,20 @@ static image<uchar> *imageRGBtoGRAY(image<rgb> *input) {
 	 imRef(input, x, y).g * GREEN_WEIGHT +
 	 imRef(input, x, y).b * BLUE_WEIGHT);
     }
+  }
+  return output;
+}
+
+static image<rgb> *imageGRAYtoRGB(image<uchar> *input) {
+  int width = input->width();
+  int height = input->height();
+  image<rgb> *output = new image<rgb>(width, height, false);
+
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      imRef(output, x, y).r = imRef(input, x, y);
+      imRef(output, x, y).g = imRef(input, x, y);
+      imRef(output, x, y).b = imRef(input, x, y);
+    }
+  }
+  return output;  
