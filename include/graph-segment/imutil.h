7ace6be6672eaf05e3a7016e2f6e2ef7b107bbc9
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2020-12-04 19:58:24 +0000

    Add migration script

--- include/graph-segment/imutil.h
+++ include/graph-segment/imutil.h
@@ -2,4 +2,4 @@
 Copyright (C) 2006 Pedro Felzenszwalb
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Genera
+it under the terms of the GNU General Public License as p
