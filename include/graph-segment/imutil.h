55867fe02cb638a373a4473baf1b706de536bab1
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2020-12-31 11:43:59 +0000

    fix sysvfs symlinks

--- include/graph-segment/imutil.h
+++ include/graph-segment/imutil.h
@@ -27,4 +27,5 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 /* compute minimum and maximum value in an image */
 template <class T>
 void min_max(image<T> *im, T *ret_min, T *ret_max) {
-  int width = im->width(
+  int width = im->width();
+  int height = i
