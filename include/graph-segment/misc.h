87ea3a22ec74f9437ed46d0958db75aa17bf290a
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2021-03-20 09:41:06 +0000

    ver up

--- include/graph-segment/misc.h
+++ include/graph-segment/misc.h
@@ -27,4 +27,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #define M_PI 3.141592653589793
 #endif
 
-typedef unsigned char
+typedef unsigned char uchar;
+
+typedef struct { uchar
