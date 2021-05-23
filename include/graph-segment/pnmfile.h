2f3a7cee8bf5a3280e6b0cb05f557daf2a11c926
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2021-05-23 15:26:29 +0000

    diagram

--- include/graph-segment/pnmfile.h
+++ include/graph-segment/pnmfile.h
@@ -16,4 +16,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 */
 
-/* basic image I/O 
+/* basic image I/O */
+
+#ifndef PNM_FILE_H
