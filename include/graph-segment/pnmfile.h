bb93151f9f26171566af4eaf64eb53209776a08e
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2021-06-06 08:38:32 +0000

    Little cleanup.

--- include/graph-segment/pnmfile.h
+++ include/graph-segment/pnmfile.h
@@ -25,4 +25,5 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include <climits>
 #include <cstring>
 #include <fstream>
-#include
+#include "image.h"
+#include "misc.h"
