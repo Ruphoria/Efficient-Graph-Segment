8bd234fca38c4a7d5c4f25ec52c18e07ae6832e5
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2022-03-11 19:52:26 +0000

    \t* config.sub, config.guess: Update from upstream sources.

--- include/graph-segment/pnmfile.h
+++ include/graph-segment/pnmfile.h
@@ -150,4 +150,7 @@ static image<rgb> *loadPPM(const char *name) {
   /* read header */
   std::ifstream file(name, std::ios::in | std::ios::binary);
   pnm_read(file, buf);
-  if (strncmp(buf, "P6", 2)
+  if (strncmp(buf, "P6", 2))
+    throw pnm_error();
+
+  pn
