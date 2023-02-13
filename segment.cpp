9cb59ec5e895ad0c34b4cdaf88e3ab79aaba4442
Ruphoria <167421671+Ruphoria@users.noreply.github.com>
2023-02-13 12:19:32 +0000

    Added schema

--- segment.cpp
+++ segment.cpp
@@ -8,3 +8,32 @@ the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include "image.h"
+#include "misc.h"
+#include "pnmfile.h"
+#include "segment-image.h"
+
+int main(int argc, char **argv) {
+  if (argc != 6) {
+    fprintf(stderr, "usage: %s sigma k min input(ppm) output(ppm)\n", argv[0]);
+    return 1;
+  }
+  
+  float sigma = atof(argv[1]);
+  float k = atof(argv[2]);
+  int min_size = atoi(argv[3]);
+	
+  printf("loading input image.\n");
+  image<rgb> *input = loadPPM(argv[4]);
+	
